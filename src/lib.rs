afc68f22143e8ee275e97fe1cbfff2a1c9eff5b4
Don Hardman <don.hardman@muvon.io>
2022-11-18 05:00:47 +0000

    Use Muvon\Ext\VarInt for class name, introduce exceptions and rewrite tests to use macros

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 #![cfg_attr(windows, feature(abi_vectorcall))]
-use ext_php_rs::{prelude::*, binary::Binary};
-use faster_hex::{hex_string, hex_decode_fallback};
-use std::str;
+use ext_php_rs::{prelude::*, binary::Binary, zend::ce};
+use faster_hex::{hex_string, hex_decode};
 
-#[php_class]
+#[php_class(name = "Muvon\\Ext\\VarIntError")]
+#[extends(ce::exception())]
+pub struct VarIntError;
+
+#[php_class(name = "Muvon\\Ext\\VarInt")]
 pub struct VarInt;
 
 #[php_impl(rename_methods = "camelCase")]
@@ -42,7 +45,7 @@ impl VarInt {
     }
 
     #[public]
-    pub fn read_uint(bin: Binary<u8>, offset: Option<u64>, max_len: Option<u64>) -> Vec<u64> {
+    pub fn read_uint(bin: Binary<u8>, offset: Option<u64>, max_len: Option<u64>) -> Result<Vec<u64>, PhpException> {
         let offset = offset.unwrap_or(0);
         let max_len = max_len.unwrap_or(0);
 
@@ -51,33 +54,31 @@ impl VarInt {
         let mut i: u64 = offset;
         let max_i = max_len + offset;
 
-        // let chars: Vec<char> = bin.chars().collect();
         for u in bin.iter() {
             let u = u.to_owned();
             if u < 0x80 {
                 if max_len > 0 && (i > max_i || i == max_i && u > 1) {
-                    panic!("error");
+                    return Err(PhpException::from_class::<VarIntError>("Failed to parse".into()));
                 }
-                return vec![(x | (u as u64) << s) as u64, i + 1];
+                return Ok(vec![(x | (u as u64) << s) as u64, i + 1]);
             }
             x = x | ((u & 0x7f) as u64) << s;
             s += 7;
             i += 1;
         }
 
-        vec![0, 0]
+        Ok(vec![0, 0])
     }
 
     #[public]
-    pub fn read_uint_hex(hex: String, offset: Option<u64>, max_len: Option<u64>) -> Vec<u64> {
-        let mut dst = Vec::with_capacity(hex.len() / 2);
-        hex_decode_fallback(hex.as_bytes(), &mut dst);
-        Self::read_uint(Binary::new(dst), offset, max_len)
+    pub fn read_uint_hex(hex: String, offset: Option<u64>, max_len: Option<u64>) -> Result<Vec<u64>, PhpException> {
+        let bin = Self::try_hex_to_binary(hex)?;
+        Self::read_uint(bin, offset, max_len)
     }
 
     #[public]
-    pub fn read_int(bin: Binary<u8>, offset: Option<u64>, max_len: Option<u64>) -> Vec<i64> {
-        let input = Self::read_uint(bin, offset, max_len);
+    pub fn read_int(bin: Binary<u8>, offset: Option<u64>, max_len: Option<u64>) -> Result<Vec<i64>, PhpException> {
+        let input = Self::read_uint(bin, offset, max_len)?;
         let (ux, offset) = match input[..] {
             [a, b] => (a, b),
             _ => unreachable!(),
@@ -89,26 +90,35 @@ impl VarInt {
             x = !x;
         }
 
-        vec![x, offset as i64]
+        Ok(vec![x, offset as i64])
     }
 
     #[public]
-    pub fn read_int_hex(hex: String, offset: Option<u64>, max_len: Option<u64>) -> Vec<i64> {
-        let mut dst = Vec::with_capacity(hex.len() / 2);
-        hex_decode_fallback(hex.as_bytes(), &mut dst);
-        Self::read_int(Binary::new(dst), offset, max_len)
+    pub fn read_int_hex(hex: String, offset: Option<u64>, max_len: Option<u64>) -> Result<Vec<i64>, PhpException> {
+        let bin = Self::try_hex_to_binary(hex)?;
+        Self::read_int(bin, offset, max_len)
     }
 
     #[public]
-    pub fn read_bool(bin: Binary<u8>, offset: Option<u64>) -> Vec<u64> {
+    pub fn read_bool(bin: Binary<u8>, offset: Option<u64>) -> Result<Vec<u64>, PhpException> {
         Self::read_uint(bin, offset, Some(0u64))
     }
 
     #[public]
-    pub fn read_bool_hex(hex: String, offset: Option<u64>) -> Vec<u64> {
-        let mut dst = Vec::with_capacity(hex.len() / 2);
-        hex_decode_fallback(hex.as_bytes(), &mut dst);
-        Self::read_bool(Binary::new(dst), offset)
+    pub fn read_bool_hex(hex: String, offset: Option<u64>) -> Result<Vec<u64>, PhpException> {
+        let bin = Self::try_hex_to_binary(hex)?;
+        Self::read_bool(bin, offset)
+    }
+
+    fn try_hex_to_binary(hex: String) -> Result<Binary<u8>, PhpException> {
+        let len = hex.len() / 2;
+        let mut dst = Vec::with_capacity(len);
+        dst.resize(len, 0);
+        match hex_decode(hex.as_bytes(), &mut dst) {
+            Ok(_) => (),
+            Err(_) => return Err(PhpException::from_class::<VarIntError>("Failed to decode hex input".into())),
+        };
+        Ok(Binary::new(dst))
     }
 }
 
@@ -123,27 +133,27 @@ pub fn varint_pack_int(value: i64) -> Binary<u8> {
 }
 
 #[php_function]
-pub fn varint_read_uint(bin: Binary<u8>, offset: Option<u64>, max_len: Option<u64>) -> Vec<u64> {
+pub fn varint_read_uint(bin: Binary<u8>, offset: Option<u64>, max_len: Option<u64>) -> Result<Vec<u64>, PhpException> {
     VarInt::read_uint(bin, offset, max_len)
 }
 
 #[php_function]
-pub fn varint_read_int(bin: Binary<u8>, offset: Option<u64>, max_len: Option<u64>) -> Vec<i64> {
+pub fn varint_read_int(bin: Binary<u8>, offset: Option<u64>, max_len: Option<u64>) -> Result<Vec<i64>, PhpException> {
     VarInt::read_int(bin, offset, max_len)
 }
 
 #[php_function]
-pub fn varint_read_int_hex(hex: String, offset: Option<u64>, max_len: Option<u64>) -> Vec<i64> {
+pub fn varint_read_int_hex(hex: String, offset: Option<u64>, max_len: Option<u64>) -> Result<Vec<i64>, PhpException> {
     VarInt::read_int_hex(hex, offset, max_len)
 }
 
 #[php_function]
-pub fn varint_read_bool(bin: Binary<u8>, offset: Option<u64>) -> Vec<u64> {
+pub fn varint_read_bool(bin: Binary<u8>, offset: Option<u64>) -> Result<Vec<u64>, PhpException> {
     VarInt::read_bool(bin, offset)
 }
 
 #[php_function]
-pub fn varint_read_bool_hex(hex: String, offset: Option<u64>) -> Vec<u64> {
+pub fn varint_read_bool_hex(hex: String, offset: Option<u64>) -> Result<Vec<u64>, PhpException> {
     VarInt::read_bool_hex(hex, offset)
 }
 
@@ -154,58 +164,50 @@ pub fn get_module(module: ModuleBuilder) -> ModuleBuilder {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
+
     use super::*;
 
+    macro_rules! test_fn {
+        ($pack_fn:ident, $read_fn:ident, $tests:expr) => {
+            for (input, expected) in $tests {
+                assert_eq!(
+                    VarInt::$pack_fn(input),
+                    expected
+                );
+
+                if let Ok(result) = VarInt::$read_fn(expected.into(), Some(0), Some(0)) {
+                    assert_eq!(result[0], input);
+                } else {
+                    panic!("error");
+                }
+            }
+        };
+    }
     #[test]
-    pub fn test_pack_zero_unsigned_int() -> () {
-        assert_eq!(
-            VarInt::pack_uint_hex(0),
-            "00"
-        );
+    pub fn test_pack_and_unpack_unsigned_ints() -> () {
+        let tests = HashMap::from([
+            (0, "00"),
+            (35442, "f29402"),
+            (523472346, "da9bcef901"),
+            (23425234862394, "ba82b6e6e1a905"),
+            (7342523486232352394, "8aaddebed5c6f8f265"),
+        ]);
+
+        test_fn!(pack_uint_hex, read_uint_hex, tests);
     }
 
-    #[test]
-    pub fn test_pack_non_zero_unsigned_ints() -> () {
-        assert_eq!(
-            VarInt::pack_uint_hex(35442),
-            "f29402"
-        );
-
-        assert_eq!(
-            VarInt::pack_uint_hex(523472346),
-            "da9bcef901"
-        );
-
-        assert_eq!(
-            VarInt::pack_uint_hex(23425234862394),
-            "ba82b6e6e1a905"
-        );
-    }
-
 
     #[test]
-    pub fn test_pack_zero_signed_int() -> () {
-        assert_eq!(
-            VarInt::pack_int_hex(0),
-            "00"
-        );
-    }
+    pub fn test_pack_and_unpack_zero_signed_int() -> () {
+        let tests = HashMap::from([
+            (0, "00"),
+            (35442, "e4a904"),
+            (-523472346, "b3b79cf303"),
+            (-34235, "f59604"),
+            (-5234723234233423446, "abd9bc9fffc2bca59101"),
+        ]);
 
-    #[test]
-    pub fn test_pack_non_zero_signed_ints() -> () {
-        assert_eq!(
-            VarInt::pack_int_hex(35442),
-            "e4a904"
-        );
-
-        assert_eq!(
-            VarInt::pack_int_hex(-523472346),
-            "b3b79cf303"
-        );
-
-        assert_eq!(
-            VarInt::pack_int_hex(-34235),
-            "f59604"
-        );
+        test_fn!(pack_int_hex, read_int_hex, tests);
     }
 }
